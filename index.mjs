bfd12a7c5ca425aa4c34b4fe130072464bb8c48a
alihuzaifa <ha4587323@gmail.com>
2023-01-21 15:24:42 +0500

    adding new code of my website

--- index.mjs
+++ index.mjs
@@ -1,11 +1,8 @@
 import express from "express";
+import path from "path";
+// import { __dirname } from "path";
 const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  console.log(`Some is getting message  by ${req.ip}`);
-  res.send("Hello World!");
-});
+const port = process.env.PORT || 5000;
 
 app.get("/home", (req, res) => {
   console.log(`Some is getting message at home section by  ${req.ip}`);
@@ -15,6 +12,15 @@ app.get("/home", (req, res) => {
 app.get("/about", (req, res) => {
   console.log(`Some is getting message at about section by  ${req.ip}`);
   res.send("Hello World About!");
+});
+
+const __dirname = path.resolve();
+app.use("/", express.static(path.join(__dirname, "./practicing/build")));
+app.use("*", express.static(path.join(__dirname, "./practicing/build")));
+
+app.get("/", (req, res) => {
+  console.log(`Some is getting message  by ${req.ip}`);
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
