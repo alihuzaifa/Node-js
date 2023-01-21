a5b14f11fa151ace7c907ee555c2ece9b81d5b27
alihuzaifa <ha4587323@gmail.com>
2023-01-21 09:09:12 +0500

    practicing of deploying a server

---
+++ index.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log(`Some is getting message  by ${req.ip}`);
+  res.send("Hello World!");
+});
+
+app.get("/home", (req, res) => {
+  console.log(`Some is getting message at home section by  ${req.ip}`);
+  res.send("Hello World Home!");
+});
+
+app.get("/about", (req, res) => {
+  console.log(`Some is getting message at about section by  ${req.ip}`);
+  res.send("Hello World About!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
